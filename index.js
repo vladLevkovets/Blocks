d5a09e6f6da1418433eaca62cabc9ec3e6a1793b
vladLevkovets <vl.vladisla@gmail.com>
2022-08-29 23:22:04 +0200

    2 blocks

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const All=[{quest:"1234"},{quest:"5678"},{quest:"5678"},{quest:"5678"}]
 debugger
+
 const renderAll=()=>{
+    createScreen();
     All.forEach(ele => {
-        document.getElementById("screen").innerHTML="";
-        obj=ele
+        // createScreen(ele);
         createQ();
-        createA()
-        createSpan();
+        createA();
+        // createSpan();
       }); 
 }
+
+
+
+
 const createScreen=()=>{
   const div=document.createElement("div");
   div.setAttribute("class", "screen");
   div.setAttribute("id", "screen")
   main.appendChild(div);
-  createQ();
-  createA();
+//   createQ();
+//   createA();
   
 }
 
@@ -23,7 +28,7 @@ const createQ=()=>{
     const Q=document.createElement("div")
     Q.setAttribute("class", "block");
     Q.setAttribute("id", "")
-    Q.innerText= obj.quest
+    // Q.innerText= ele.quest
     document.getElementById("screen").appendChild(Q);
 }
 
@@ -31,20 +36,22 @@ const createA=()=>{
     const A=document.createElement("div")
     A.setAttribute("class", "block");
     A.setAttribute("id", "")
-    A.innerText= obj.quest
-    document.getElementById("screen").appendChild(Q);
+    // A.innerText= ele.quest
+    document.getElementById("screen").appendChild(A);
 }
 
 
 
 
-const createSpan=()=>{
-const span=document.createElement("span");
-span.innerText = obj.quest;
-span.setAttribute("class", classN);
-return parent.appendChild(span);
+// const createSpan=()=>{
+// const span=document.createElement("span");
+// span.innerText = ele.quest;
+// span.setAttribute("class", classN);
+// return parent.appendChild(span);
 
-}
+// }
 const createInput=()=>{
     Input.innerHTML=""
 }
+
+// renderAll();
